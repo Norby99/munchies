aef89922a1c1d11a6750e8f7278c1f18d4d35cc1
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-02-13 18:20:13 +0100

    fix: ktlint applies to root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,27 @@ import com.diffplug.gradle.spotless.SpotlessExtension
 import com.diffplug.gradle.spotless.SpotlessPlugin
 
 plugins {
-    alias    (libs.plugins.spotless)     apply false
+  alias(libs.plugins.spotless) apply false
 }
 
-subprojects {
-    apply<SpotlessPlugin>()
-    configure<SpotlessExtension>{
-        kotlin {
-            target    ("**/*.kt")
-            targetExclude("**/build/**/*.kt")
-            ktlint()
-        }
-        kotlinGradle {
-            target("**/*.kt")
-            targetExclude("**/build/**/*.kt")
-            ktlint()
-        }
+fun configureSpotlessForKotlin(project: Project) {
+  project.configure<SpotlessExtension> {
+    kotlin {
+      target("**/*.kt")
+      targetExclude("**/build/**/*.kt")
+      ktlint()
     }
+    kotlinGradle {
+      target("**/*.kts")
+      targetExclude("**/build/**/*.kts")
+      ktlint()
+    }
+  }
+}
+apply<SpotlessPlugin>()
+configureSpotlessForKotlin(rootProject)
+
+subprojects {
+  apply<SpotlessPlugin>()
+  configureSpotlessForKotlin(this)
 }
