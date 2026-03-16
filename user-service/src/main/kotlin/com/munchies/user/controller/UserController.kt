af87de904fda63d06d5e77a220194d335593f6b8
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-03-16 17:18:02 +0100

    build: removed openapi generation

---
+++ user-service/src/main/kotlin/com/munchies/user/controller/UserController.kt
@@ -0,0 +1,17 @@
+package com.munchies.user.controller
+
+import com.munchies.user.api.UserApi
+import com.munchies.user.domain.UserId
+import com.munchies.user.dto.UserDTO
+import com.munchies.user.dto.toDTO
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.PathVariable
+
+@Controller
+class UserController(private val service: UserService) : UserApi<String, UserDTO> {
+  @Get("/users/{id}/")
+  override fun getUser(@PathVariable id: String): UserDTO = service
+    .getUser(UserId(id))
+    .toDTO()
+}
