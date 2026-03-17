c0abc5423aba84f8c0e19c30dfa520f082a2bcd8
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-03-17 09:30:37 +0100

    refactor: preparations for splitting service and api

--- user-service/src/main/kotlin/com/munchies/user/dto/DomainToObject.kt
+++ user-service/src/main/kotlin/com/munchies/user/dto/DomainToObject.kt
@@ -1,11 +1,8 @@
 package com.munchies.user.dto
 
+import com.munchies.user.api.UserDTO
 import com.munchies.user.domain.User
 import com.munchies.user.domain.UserId
-import io.micronaut.serde.annotation.Serdeable
-
-@Serdeable
-class UserDTO(val id: String)
 
 fun UserDTO.toDomain(): User = User(id = UserId(id))
 fun User.toDTO(): UserDTO = UserDTO(id = id.value)
