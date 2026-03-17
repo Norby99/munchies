ee67ed769d02b46e0c09edb8178dd9f86d75fde8
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-03-17 16:55:02 +0100

    fix: user id was not correct

--- user-service/service/src/main/kotlin/com/munchies/user/domain/User.kt
+++ user-service/service/src/main/kotlin/com/munchies/user/domain/User.kt
@@ -2,4 +2,4 @@ package com.munchies.user.domain
 
 import com.munchies.commons.AggregateRoot
 
-open class User(override val id: UserId) : AggregateRoot<UserId>(id = UserId())
+open class User(override val id: UserId) : AggregateRoot<UserId>(id = id)
