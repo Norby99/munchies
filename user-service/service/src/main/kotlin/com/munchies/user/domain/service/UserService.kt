01a4edd239f732f054f18881fe2f9ca03c93f2aa
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-03-20 18:25:42 +0100

    refactor: split into hexagonal architecture

--- user-service/service/src/main/kotlin/com/munchies/user/domain/service/UserService.kt
+++ user-service/service/src/main/kotlin/com/munchies/user/domain/service/UserService.kt
@@ -1,8 +1,10 @@
 package com.munchies.user.domain.service
 
+import com.munchies.user.domain.model.User
 import com.munchies.user.domain.model.UserId
 import com.munchies.user.domain.port.UserRepository
+import com.munchies.user.infrastructure.api.UserApi
 
-open class UserService(private val userRepository: UserRepository) {
-  fun getUser(id: UserId) = userRepository.findById(id)
+open class UserService(private val userRepository: UserRepository) : UserApi<UserId, User?> {
+  override fun getUser(id: UserId): User? = userRepository.findById(id)
 }
--- user-service/service/src/main/kotlin/com/munchies/user/infrastructure/controller/UserController.kt
+++ user-service/service/src/main/kotlin/com/munchies/user/infrastructure/controller/UserController.kt
@@ -1,12 +1,14 @@
-package com.munchies.user.controller
+package com.munchies.user.infrastructure.controller
 
 import com.munchies.user.domain.model.UserId
-import com.munchies.user.dto.toDTO
-import com.munchies.user.infrastructure.api.UserApi
+import com.munchies.user.infrastructure.api.UserClient
 import com.munchies.user.infrastructure.api.dto.UserDTO
 import com.munchies.user.infrastructure.config.UserServiceConfig
 import com.munchies.user.infrastructure.service.Service
+import com.munchies.user.presentation.toDTO
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Error
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.serde.annotation.SerdeImport
@@ -17,8 +19,13 @@ import jakarta.inject.Inject
 class UserController(
   @Inject
   private val service: Service.ControllerService,
-) : UserApi<String, UserDTO> {
+) : UserClient {
   @Get("/users/{id}/")
-  override fun getUser(@PathVariable id: String): UserDTO = service
-    .getUser(UserId(id))?.toDTO() ?: UserDTO(id)
+  override fun getUser(@PathVariable id: String): HttpResponse<UserDTO> {
+    val user = service.getUser(UserId(id)) ?: throw UserClient.Companion.UserNotFoundException(id)
+    return HttpResponse.ok(user.toDTO())
+  }
+
+  @Error(exception = UserClient.Companion.UserNotFoundException::class)
+  override fun handleUserNotFound(): HttpResponse<Void> = HttpResponse.notFound()
 }
