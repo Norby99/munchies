1ed45c6dc13ca0e6fac4326128d9885d8c650cc9
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-03-25 16:32:56 +0100

    fix: User is not extendable

--- user-service/service/src/main/kotlin/com/munchies/user/domain/model/User.kt
+++ user-service/service/src/main/kotlin/com/munchies/user/domain/model/User.kt
@@ -2,4 +2,4 @@ package com.munchies.user.domain.model
 
 import com.munchies.commons.AggregateRoot
 
-open class User(override val id: UserId) : AggregateRoot<UserId>(id = id)
+data class User(override val id: UserId) : AggregateRoot<UserId>(id = id)
