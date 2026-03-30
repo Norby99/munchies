11e869e4b65ae74725ba2c7bf5734af101ad8ef8
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-03-30 17:50:40 +0200

    feat: add domain classes, dtos and mappers

--- suggestion-service/service/src/main/kotlin/com/munchies/suggestion/domain/model/SuggestionResponse.kt
+++ suggestion-service/service/src/main/kotlin/com/munchies/suggestion/domain/model/SuggestionResponse.kt
@@ -3,10 +3,5 @@ package com.munchies.suggestion.domain.model
 data class SuggestionResponse(
   val rationale: String,
   val confidence: SuggestionConfidence,
-  val suggestedMenuItems: List<SuggestedMenuItems>,
-)
-
-data class SuggestedMenuItems(
-  val itemId: Int,
-  val reason: String,
+  val suggestedMenuItems: List<SuggestedMenuItem>,
 )
