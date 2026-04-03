0a77516e859206d3d89968d44bbb8982f235acb8
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-04-03 15:18:53 +0200

    build: preparations for redoc openapi site

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import com.diffplug.gradle.spotless.SpotlessExtension
 import com.diffplug.gradle.spotless.SpotlessPlugin
+import utils.getServiceName
 
 plugins {
   alias(libs.plugins.spotless) apply false
@@ -30,6 +31,32 @@ configureSpotlessForKotlin(rootProject)
 subprojects {
   apply<SpotlessPlugin>()
   configureSpotlessForKotlin(this)
+}
 
-  apply(plugin = "munchies-subproject")
+tasks.register("prepareOpenApiSpecs") {
+  val serviceProjects = subprojects.filter { it.name.matches(Regex("service")) }
+
+  dependsOn(serviceProjects.map { "${it.path}:build" })
+
+  val specSources = serviceProjects.map { subproject ->
+    val sourcePath = subproject.layout.buildDirectory
+      .dir("generated/ksp/main/resources/META-INF/swagger")
+      .get().asFile
+    val targetName = "${getServiceName(subproject.parent!!)}.yml"
+    sourcePath to targetName
+  }
+  val outputDir = rootProject.layout.buildDirectory.dir("openapi/").get().asFile
+
+  inputs.files(specSources.map { (dir, _) -> dir })
+  outputs.dir(outputDir)
+
+  doLast {
+    if (outputDir.exists().not()) outputDir.mkdir()
+    specSources.forEach { (sourceDir, targetName) ->
+      sourceDir.listFiles { f -> f.extension == "yml" }
+        ?.forEach { file ->
+          file.copyTo(outputDir.resolve(targetName), overwrite = true)
+        }
+    }
+  }
 }
