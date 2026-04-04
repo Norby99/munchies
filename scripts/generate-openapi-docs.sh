0cc82833b6be4b22d6a55da98be23994a4558365
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-04-04 11:49:13 +0200

    build: dynamic api site generation

--- scripts/generate-openapi-docs.sh
+++ scripts/generate-openapi-docs.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
+SPEC_NAMES = ()
+
 for file in $(cd ./build/openapi/ && find .  -name "*.yml"); do
-  echo "$file"
   base_name=${file%.yml}
-  echo "$base_name"
   npx @redocly/cli build-docs "./build/openapi/$file" -o "./docs/pages/openapi/$base_name/index.html"
+  SPEC_NAMES+=("$base_name")
+done
+
+for spec in "${SPECS_DIR[@]}}"; do
+  echo "- [$spec-service](./docs/pages/openapi/$spec/index.html)" >> ./docs/pages/openapi/index.md
 done
