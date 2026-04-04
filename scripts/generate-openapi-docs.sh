cef39567c70717b099e577f372b9751b0c0c01d6
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-04-04 12:23:07 +0200

    fix: OpenAPI specs dynamically injected into the index

--- scripts/generate-openapi-docs.sh
+++ scripts/generate-openapi-docs.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 
-SPEC_NAMES = ()
+SPEC_NAMES=()
 
 for file in $(cd ./build/openapi/ && find .  -name "*.yml"); do
   base_name=${file%.yml}
   npx @redocly/cli build-docs "./build/openapi/$file" -o "./docs/pages/openapi/$base_name/index.html"
-  SPEC_NAMES+=("$base_name")
+  spec_name=${base_name#./}
+  echo "$spec_name"
+  SPEC_NAMES+=("$spec_name")
 done
 
-for spec in "${SPECS_DIR[@]}}"; do
-  echo "- [$spec-service](./docs/pages/openapi/$spec/index.html)" >> ./docs/pages/openapi/index.md
+for spec in "${SPEC_NAMES[@]}"; do
+  printf '\n' >> ./docs/pages/openapi/index.md
+  echo "- [$spec-service](/docs/pages/openapi/$spec/index.html)" >> ./docs/pages/openapi/index.md
 done
