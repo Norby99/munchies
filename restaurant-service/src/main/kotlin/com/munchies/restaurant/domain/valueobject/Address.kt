2f6a033931958f18e7ebb558707a91104aa0ed82
erattini <emirattinee@gmail.com>
2026-04-07 17:22:57 +0200

    fix: correct blank address error message

--- restaurant-service/src/main/kotlin/com/munchies/restaurant/domain/valueobject/Address.kt
+++ restaurant-service/src/main/kotlin/com/munchies/restaurant/domain/valueobject/Address.kt
@@ -3,9 +3,9 @@ package com.munchies.restaurant.domain.valueobject
 @JvmInline
 value class Address private constructor(val value: String) {
   init {
-    require(value.isNotBlank()) { "Restaurant address cannot be blank" }
+    require(value.isNotBlank()) { "Address cannot be blank" }
     require(value.length <= MAX_ADDRESS_LENGTH) {
-      "Restaurant address cannot exceed $MAX_ADDRESS_LENGTH characters"
+      "Address cannot exceed $MAX_ADDRESS_LENGTH characters"
     }
   }
 
