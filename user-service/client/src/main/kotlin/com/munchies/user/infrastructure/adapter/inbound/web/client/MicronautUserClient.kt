fbb712c89dbf09df59fa318b45f59a09c473b25c
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-04-07 15:35:44 +0200

    docs: add user api documentation

--- user-service/client/src/main/kotlin/com/munchies/user/infrastructure/adapter/inbound/web/client/MicronautUserClient.kt
+++ user-service/client/src/main/kotlin/com/munchies/user/infrastructure/adapter/inbound/web/client/MicronautUserClient.kt
@@ -10,16 +10,37 @@ import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.annotation.Client
 
+/**
+ * Micronaut HTTP client interface for interacting with the User service.
+ */
 @Client(path = UserServiceConfig.SERVICE_PATH)
 sealed interface MicronautUserClient {
 
   companion object {
+    /**
+     * Client interface for retrieving user details.
+     */
     interface MicronautGetUser : GetUser<String, HttpResponse<UserDTO>>, MicronautUserClient {
+      /**
+       * Retrieves a user by their unique identifier.
+       *
+       * @param id The unique identifier of the user to retrieve.
+       * @return An [HttpResponse] containing the [UserDTO] if found.
+       */
       @Get("{id}/")
       @SingleResult
       override fun getUser(id: String): HttpResponse<UserDTO>
     }
+
+    /**
+     * Client interface for adding a new user.
+     */
     interface MicronautAddUser : AddUser<HttpResponse<String>>, MicronautUserClient {
+      /**
+       * Submits a request to add a new user.
+       *
+       * @return An [HttpResponse] containing the created user identifier or confirmation message.
+       */
       @Post("/")
       override fun addUser(): HttpResponse<String>
     }
--- user-service/shared/src/main/kotlin/com/munchies/user/infrastructure/adapter/inbound/UserAPI.kt
+++ user-service/shared/src/main/kotlin/com/munchies/user/infrastructure/adapter/inbound/UserAPI.kt
@@ -1,10 +1,26 @@
 package com.munchies.user.infrastructure.adapter.inbound
 
+/**
+ * This interface defines the API for user-related operations.
+ * It is a sealed interface that contains two sub-interfaces:
+ * - GetUser: Defines a method to retrieve a user by their ID.
+ * - AddUser: Defines a method to add a new user and return their ID.
+ */
 sealed interface UserAPI {
   companion object {
+    /**
+     * The GetUser interface defines a method to retrieve a user by their ID.
+     * @param UserId The type of the user ID.
+     * @param User The type of the user object.
+     */
     interface GetUser<UserId, User> : UserAPI {
       fun getUser(id: UserId): User
     }
+
+    /**
+     * The AddUser interface defines a method to add a new user and return their ID.
+     * @param UserId The type of the user ID.
+     */
     interface AddUser<UserId> : UserAPI {
       fun addUser(): UserId
     }
