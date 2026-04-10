22d7171ee5fb49f80e58ef30a8a3ca355d674dd7
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-04-10 18:00:47 +0200

    docs: fix docs

--- user-service/service/src/main/kotlin/com/munchies/user/domain/model/UserCredentials.kt
+++ user-service/service/src/main/kotlin/com/munchies/user/domain/model/UserCredentials.kt
@@ -9,8 +9,10 @@ import com.munchies.commons.Entity
  * @property passwordHash The hashed password of the user, used for authentication.
  * @property salt The cryptographic salt used during password hashing for added security.
  * @property loginAttempts The number of failed login attempts. Defaults to 0.
- * @property lockedUntil The timestamp (in milliseconds) until which the account is locked. Defaults to -1L, indicating no lock.
- * @property lastLogin The timestamp (in milliseconds) of the user's last successful login. Defaults to 0L.
+ * @property lockedUntil The timestamp (in milliseconds) until which the account is locked.
+ * Defaults to -1L, indicating no lock.
+ * @property lastLogin The timestamp (in milliseconds) of the user's last successful login.
+ * Defaults to 0L.
  */
 data class UserCredentials(
   override val id: UserId,
