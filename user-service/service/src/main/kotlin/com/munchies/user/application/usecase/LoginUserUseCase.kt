bb27d0c56a9b068997c3ac9de9c16d9653cf2e82
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-04-12 15:45:15 +0200

    refactor: time provider

--- user-service/service/src/main/kotlin/com/munchies/user/application/usecase/LoginUserUseCase.kt
+++ user-service/service/src/main/kotlin/com/munchies/user/application/usecase/LoginUserUseCase.kt
@@ -8,6 +8,7 @@ import com.munchies.user.application.port.inbound.LoginUser.Companion.LoginResul
 import com.munchies.user.domain.model.User
 import com.munchies.user.domain.model.UserCredentials
 import com.munchies.user.domain.port.PasswordHasher
+import com.munchies.user.domain.port.TimeProvider
 import com.munchies.user.domain.port.UserCredentialsRepository
 import com.munchies.user.domain.port.UserRepository
 
@@ -15,7 +16,7 @@ class LoginUserUseCase(
   private val userRepository: UserRepository,
   private val credentialsRepository: UserCredentialsRepository,
   private val passwordHasher: PasswordHasher,
-  private val timeProvider: () -> Long,
+  private val timeProvider: TimeProvider,
 ) : LoginUser {
 
   private fun findUser(email: String, username: String): User? = when {
