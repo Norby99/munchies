faafb171ff2acd6a6eb836fbb6505c4e13e2eaf4
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-04-13 13:48:36 +0200

    fix: requests validation

--- user-service/shared/src/main/kotlin/com/munchies/user/infrastructure/adapter/inbound/request/LoginUserRequest.kt
+++ user-service/shared/src/main/kotlin/com/munchies/user/infrastructure/adapter/inbound/request/LoginUserRequest.kt
@@ -4,4 +4,15 @@ data class LoginUserRequest(
   val email: String,
   val username: String,
   val password: String,
-)
+) {
+  companion object {
+    @Throws(IllegalArgumentException::class)
+    fun validate(request: LoginUserRequest) {
+      require(
+        request.email.isNotBlank() ||
+          request.username.isNotEmpty(),
+      ) { "Email or Username must not be blank" }
+      require(request.password.isNotBlank()) { "Password must not be blank" }
+    }
+  }
+}
