7e3afb25f6314a654b563db2bd0d655e11832194
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-04-13 14:57:57 +0200

    fix: shared endpoints paths

--- user-service/shared/src/main/kotlin/com/munchies/user/infrastructure/adapter/inbound/web/config/UserServiceConfig.kt
+++ user-service/shared/src/main/kotlin/com/munchies/user/infrastructure/adapter/inbound/web/config/UserServiceConfig.kt
@@ -3,5 +3,10 @@ package com.munchies.user.infrastructure.adapter.inbound.web.config
 object UserServiceConfig {
   const val SERVICE_NAME = "user-service"
   const val SERVICE_PATH = "/users/"
+  const val GET_USER_PATH = "{id}/"
+  const val REGISTER_USER_PATH = "register/"
+  const val LOGIN_USER_PATH = "login/"
+  const val UPDATE_USER_PASSWORD_PATH = "update-password/"
+  const val UPDATE_USER_INFO_PATH = "update-info/"
   const val SERVICE_PORT = 8080
 }
