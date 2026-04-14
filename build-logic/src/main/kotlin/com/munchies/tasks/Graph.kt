7261e714f51dfcd6bb1415e01182e1f0b0a108ea
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-04-14 12:10:05 +0200

    fix: dependencies graph

--- build-logic/src/main/kotlin/com/munchies/tasks/Graph.kt
+++ build-logic/src/main/kotlin/com/munchies/tasks/Graph.kt
@@ -35,7 +35,13 @@ private class Graph(
       .map { it to it.dependencies.withType<ProjectDependency>().ifEmpty { null } }
       .filter { (_, dep) -> dep != null }
       .flatMap { (c, value) -> value!!.map { dep -> c to project.project(dep.path) } }
-      .filter { (c, _) -> c.toString().contains(regex = Regex(".*.mplementation.*")) }
+      .filter { (c, _) ->
+        c.toString().contains(
+          regex = Regex(
+            ".*(?:implementation|commonMainImplementation).*",
+          ),
+        )
+      }
       .forEach { (configuration: Configuration, projectDependency: Project) ->
         val newPair = (configuration to projectDependency)
         dependencies.compute(project) { _, u -> u.orEmpty() + newPair }
