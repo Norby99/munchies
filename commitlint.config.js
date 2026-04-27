2bbe9b5237a987176b0d67c8559b94f36cc1b3c7
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-04-27 20:26:03 +0200

    build: easier commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        "header-max-length": () => [0, "always", 72],
+        "subject-case": () => [0, "never", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
+    }
+};
