6faf6ef5156a656cc7b71034fba37a2558ccfdaa
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-04-27 18:18:02 +0200

    test: coverage reports

--- payment-service/service/vitest.config.ts
+++ payment-service/service/vitest.config.ts
@@ -3,7 +3,16 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+
   test: {
     include: ["src/test/ts/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: ["src/main/ts/domain/external-modules.{ts,tsx}"],
+      all: true,
+      reporter: ["text", "lcov", "json"],
+      // thresholds: { lines: 80, functions: 80, branches: 75, statements: 80},
+    },
   },
 });
