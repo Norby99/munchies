ed0d280adea293cbbfc49fcd6d49d68285d1926c
Norby99 <gabos.norbert99@gmail.com>
2026-06-03 14:14:59 +0200

    feat(order): implement various order type

---
+++ order-service/src/main/kotlin/com/munchies/order/domain/model/DeliveryOrder.kt
@@ -0,0 +1,18 @@
+package com.munchies.order.domain.model
+
+import com.munchies.commons.Entity
+
+data class DeliveryOrder(
+  override val id: OrderId,
+  val status: OrderStatus = OrderStatus.PENDING,
+  val items: List<OrderItem>,
+  val deliveryInfo: DeliveryInfo,
+) : Entity<OrderId>(id) {
+  val type = OrderType.DELIVERY
+}
+
+data class DeliveryInfo(
+  val deliveryAddress: String,
+  val customerName: String,
+  val customerPhone: String,
+)
