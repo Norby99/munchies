b22f929ebe6a81b02704500aa4ad189de86f0617
Norby99 <gabos.norbert99@gmail.com>
2026-06-04 15:33:47 +0200

    refactor(order): extract status & items from Order subclasses

--- order-service/src/main/kotlin/com/munchies/order/domain/model/TakeawayOrder.kt
+++ order-service/src/main/kotlin/com/munchies/order/domain/model/TakeawayOrder.kt
@@ -4,10 +4,10 @@ data class TakeawayOrder(
   override val id: OrderId,
   override val restaurantId: String,
   override val customerId: String,
-  val status: OrderStatus = OrderStatus.PENDING,
-  val items: List<OrderItem>,
+  override val status: OrderStatus = OrderStatus.PENDING,
+  override val items: List<OrderItem>,
   val takeawayInfo: TakeawayInfo,
-) : Order(id, restaurantId, customerId) {
+) : Order(id, restaurantId, customerId, status, items) {
   val type = OrderType.TAKEAWAY
 }
 
