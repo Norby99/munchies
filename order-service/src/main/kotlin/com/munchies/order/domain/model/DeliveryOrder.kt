f8d2ba28ea659855ab781827d45f681e5690782f
Norby99 <gabos.norbert99@gmail.com>
2026-06-05 18:36:04 +0200

    refactor(order): OrderStatus no more defaults to Pending

--- order-service/src/main/kotlin/com/munchies/order/domain/model/DeliveryOrder.kt
+++ order-service/src/main/kotlin/com/munchies/order/domain/model/DeliveryOrder.kt
@@ -4,7 +4,7 @@ data class DeliveryOrder(
   override val id: OrderId,
   override val restaurantId: RestaurantId,
   override val customerId: CustomerId,
-  override val status: OrderStatus = OrderStatus.PENDING,
+  override val status: OrderStatus,
   override val items: List<OrderItem>,
   val deliveryInfo: DeliveryInfo,
 ) : Order(id, restaurantId, customerId, status, items)
