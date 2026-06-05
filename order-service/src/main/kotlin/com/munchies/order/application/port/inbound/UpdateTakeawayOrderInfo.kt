c91d0e11aaa9902490ca187c299909901a513a18
Norby99 <gabos.norbert99@gmail.com>
2026-06-05 18:43:19 +0200

    feat(order): implement UpdateTakeawayOrder port inbound

--- order-service/src/main/kotlin/com/munchies/order/application/port/inbound/UpdateTakeawayOrderInfo.kt
+++ order-service/src/main/kotlin/com/munchies/order/application/port/inbound/UpdateTakeawayOrderInfo.kt
@@ -1,3 +1,17 @@
 package com.munchies.order.application.port.inbound
 
-interface UpdateTakeawayOrderInfo
+import com.munchies.order.application.port.inbound.command.UpdateTakeawayOrderCommand
+
+interface UpdateTakeawayOrderInfo {
+
+  fun execute(command: UpdateTakeawayOrderCommand): Result
+
+  sealed interface Result {
+    data class Success(val orderId: String) : Result
+    sealed interface Failure : Result {
+      data object OrderNotFound : Failure
+      data object Unauthorized : Failure
+      data object DateInvalid : Failure
+    }
+  }
+}
