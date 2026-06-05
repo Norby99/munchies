74a2a15176bb66f9fd20150102b56cd1ea626d95
Norby99 <gabos.norbert99@gmail.com>
2026-06-05 19:01:28 +0200

    refactor(order): application type refactor

--- order-service/src/main/kotlin/com/munchies/order/application/port/inbound/command/UpdateDeliveryOrderCommand.kt
+++ order-service/src/main/kotlin/com/munchies/order/application/port/inbound/command/UpdateDeliveryOrderCommand.kt
@@ -1,9 +1,12 @@
 package com.munchies.order.application.port.inbound.command
 
+import com.munchies.order.domain.model.CustomerId
+import com.munchies.order.domain.model.OrderId
+
 data class UpdateDeliveryOrderCommand(
-  val orderId: String,
-  val customerId: String,
-  val estimatedDeliveryTime: String,
+  val orderId: OrderId,
+  val customerId: CustomerId,
+  val estimatedDeliveryTime: Long,
   val deliveryAddress: String,
   val bellName: String,
   val customerPhone: String,
