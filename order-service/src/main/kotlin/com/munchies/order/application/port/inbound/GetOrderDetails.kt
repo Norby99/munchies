a7e178392d7ba0430115c7d8d378b9ce65483c18
Norby99 <gabos.norbert99@gmail.com>
2026-06-05 18:00:58 +0200

    feat(order): implement GetOrderDetails port inbound

--- order-service/src/main/kotlin/com/munchies/order/application/port/inbound/GetOrderDetails.kt
+++ order-service/src/main/kotlin/com/munchies/order/application/port/inbound/GetOrderDetails.kt
@@ -1,3 +1,16 @@
 package com.munchies.order.application.port.inbound
 
-interface GetOrderDetails
+import com.munchies.order.application.port.inbound.command.GetOrderDetailsCommand
+import com.munchies.order.infrastructure.adapter.dto.OrderDto
+
+interface GetOrderDetails {
+
+  fun execute(command: GetOrderDetailsCommand): Result
+
+  sealed interface Result {
+    data class Success(val order: OrderDto) : Result
+    sealed interface Failure : Result {
+      data object DateInvalid : Result
+    }
+  }
+}
---
+++ order-service/src/main/kotlin/com/munchies/order/application/port/inbound/command/GetOrderDetailsCommand.kt
@@ -0,0 +1,5 @@
+package com.munchies.order.application.port.inbound.command
+
+import com.munchies.order.domain.model.OrderId
+
+data class GetOrderDetailsCommand(val orderId: OrderId)
