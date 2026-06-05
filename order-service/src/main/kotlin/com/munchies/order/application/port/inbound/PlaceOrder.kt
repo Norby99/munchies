2dd9875cb8e01c3a811545ac4ed4b35f7aa05602
Norby99 <gabos.norbert99@gmail.com>
2026-06-05 17:08:29 +0200

    refactor(order): rename Result

--- order-service/src/main/kotlin/com/munchies/order/application/port/inbound/PlaceOrder.kt
+++ order-service/src/main/kotlin/com/munchies/order/application/port/inbound/PlaceOrder.kt
@@ -4,10 +4,10 @@ import com.munchies.order.application.port.inbound.command.PlaceOrderCommand
 
 interface PlaceOrder {
 
-  fun execute(command: PlaceOrderCommand): CreateOrderResult
+  fun execute(command: PlaceOrderCommand): Result
 
-  sealed interface CreateOrderResult {
-    data class Success(val orderId: String) : CreateOrderResult
-    data object Failure : CreateOrderResult
+  sealed interface Result {
+    data class Success(val orderId: String) : Result
+    data object Failure : Result
   }
 }
