3d746e978bd68b0c621fbfa686c3b0bbe137e397
Norby99 <gabos.norbert99@gmail.com>
2026-06-05 18:36:46 +0200

    feat(order): implement UpdateOrderStatus port inbound

--- order-service/src/main/kotlin/com/munchies/order/application/port/inbound/UpdateOrderStatus.kt
+++ order-service/src/main/kotlin/com/munchies/order/application/port/inbound/UpdateOrderStatus.kt
@@ -1,3 +1,15 @@
 package com.munchies.order.application.port.inbound
 
-interface UpdateOrderStatus
+import com.munchies.order.application.port.inbound.command.UpdateOrderStatusCommand
+
+interface UpdateOrderStatus {
+  fun execute(command: UpdateOrderStatusCommand): Result
+
+  sealed interface Result {
+    data object Success : Result
+    sealed interface Failure : Result {
+      data object OrderNotFound : Failure
+      data object InvalidStatusTransition : Failure
+    }
+  }
+}
