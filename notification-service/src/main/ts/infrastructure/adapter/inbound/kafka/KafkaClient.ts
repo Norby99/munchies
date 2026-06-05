ec3bbdd20172cea2cf65a6dd39504ab8b77fa2a8
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-06-05 12:55:43 +0200

    fix: kafka crash on topics not initialized

--- notification-service/src/main/ts/infrastructure/adapter/inbound/kafka/KafkaClient.ts
+++ notification-service/src/main/ts/infrastructure/adapter/inbound/kafka/KafkaClient.ts
@@ -1,14 +1,30 @@
 import { Kafka, logLevel } from "kafkajs";
 
-function getKafka(topic: string): Kafka {
+async function getKafka(topic: string): Promise<Kafka> {
   const uri = process.env.KAFKA_BOOTSTRAP_SERVERS;
 
   if (!uri) throw new Error("Kafka is not online");
 
-  return new Kafka({
+  const kafka = new Kafka({
     clientId: topic,
     brokers: [uri],
     logLevel: logLevel.INFO,
   });
+
+  const admin = kafka.admin();
+
+  await admin.connect();
+
+  await admin.createTopics({
+    topics: [
+      {
+        topic: topic,
+      },
+    ],
+  });
+
+  await admin.disconnect();
+
+  return kafka;
 }
 export default getKafka;
