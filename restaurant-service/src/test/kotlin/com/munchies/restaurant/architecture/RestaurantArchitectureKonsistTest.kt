431a3e873ce59467dc012d75fe431eec9f75b815
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-06-08 14:31:27 +0200

    test: add restaurant konsist tests

---
+++ restaurant-service/src/test/kotlin/com/munchies/restaurant/architecture/RestaurantArchitectureKonsistTest.kt
@@ -0,0 +1,5 @@
+package com.munchies.restaurant.architecture
+
+import com.munchies.architecture.ArchitectureKonsistTest
+
+class RestaurantArchitectureKonsistTest : ArchitectureKonsistTest("restaurant")
