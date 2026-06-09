196eaa69525906413a6df9ed8ef3f0c1f3a12a9c
Norby99 <gabos.norbert99@gmail.com>
2026-06-09 13:11:33 +0200

    feat(order): implement GetOrderDetails usecase

---
+++ order-service/src/main/kotlin/com/munchies/order/application/usecase/GetOrderDetailsUseCase.kt
@@ -0,0 +1,22 @@
+package com.munchies.order.application.usecase
+
+import com.munchies.order.application.port.inbound.GetOrderDetails
+import com.munchies.order.application.port.inbound.GetOrderDetails.Result.*
+import com.munchies.order.application.port.inbound.GetOrderDetails.Result.Failure.*
+import com.munchies.order.application.port.inbound.command.GetOrderDetailsCommand
+import com.munchies.order.domain.model.OrderId
+import com.munchies.order.domain.ports.OrderRepository
+import com.munchies.order.infrastructure.adapter.dto.factory.OrderDTOFactory
+
+class GetOrderDetailsUseCase(private val repository: OrderRepository) : GetOrderDetails {
+  override fun execute(command: GetOrderDetailsCommand): GetOrderDetails.Result {
+    val order = repository.findById(OrderId(command.orderId))
+    return if (order != null) {
+      with(OrderDTOFactory.default) {
+        Success(order.fromDomain())
+      }
+    } else {
+      OrderNotFound
+    }
+  }
+}
