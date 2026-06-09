1b77f64109472fb7f8bfce448ba9402b8225ca63
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-06-09 17:52:11 +0200

    feat: json web token generation

--- user-service/src/main/kotlin/com/munchies/user/domain/port/TimeProvider.kt
+++ user-service/src/main/kotlin/com/munchies/user/domain/port/TimeProvider.kt
@@ -1,7 +1,10 @@
 package com.munchies.user.domain.port
 
+import java.util.*
+
 typealias TimeProvider = () -> Long
 fun defaultTimeProvider(): TimeProvider = { System.currentTimeMillis() }
 
 private const val ONE_HOUR_IN_MILLISECONDS = 60 * 60 * 1000L
-fun TimeProvider.addOneHour(): Long = this() + ONE_HOUR_IN_MILLISECONDS
+fun TimeProvider.addOneHour(): TimeProvider = { this() + ONE_HOUR_IN_MILLISECONDS }
+fun TimeProvider.toDate(): Date = Date(this())
