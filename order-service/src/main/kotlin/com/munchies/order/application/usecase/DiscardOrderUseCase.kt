9ef349d82fc1028d611a7367f0b8105cc7b7ea8a
Norby99 <gabos.norbert99@gmail.com>
2026-06-09 12:44:22 +0200

    feat(order): implement DiscardOrder usecase

---
+++ order-service/src/main/kotlin/com/munchies/order/application/usecase/DiscardOrderUseCase.kt
@@ -0,0 +1,22 @@
+package com.munchies.order.application.usecase
+
+import com.munchies.order.application.port.inbound.DiscardOrder
+import com.munchies.order.application.port.inbound.DiscardOrder.Result.*
+import com.munchies.order.application.port.inbound.DiscardOrder.Result.Failure.*
+import com.munchies.order.application.port.inbound.command.DiscardOrderCommand
+import com.munchies.order.domain.model.OrderId
+import com.munchies.order.domain.ports.OrderRepository
+
+class DiscardOrderUseCase(private val repository: OrderRepository) : DiscardOrder {
+  override fun execute(command: DiscardOrderCommand): DiscardOrder.Result {
+    val order = repository.findById(OrderId(command.orderId))
+    return when {
+      order == null -> OrderNotFound
+      order.customerId.value == command.customerId -> {
+        repository.delete(order)
+        Success
+      }
+      else -> Unauthorized
+    }
+  }
+}
