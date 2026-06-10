f6e736bec47721bc7c0bf807286cbb9a30da5cd9
Norby99 <gabos.norbert99@gmail.com>
2026-06-10 14:28:41 +0200

    refactor(order): extracted time validation to domain.model for takeawayOrder and deliveryOrder

--- order-service/src/main/kotlin/com/munchies/order/application/usecase/UpdateTakeawayOrderInfoUseCase.kt
+++ order-service/src/main/kotlin/com/munchies/order/application/usecase/UpdateTakeawayOrderInfoUseCase.kt
@@ -5,28 +5,26 @@ import com.munchies.order.application.port.inbound.UpdateTakeawayOrderInfo.Resul
 import com.munchies.order.application.port.inbound.UpdateTakeawayOrderInfo.Result.Success
 import com.munchies.order.application.port.inbound.command.UpdateTakeawayOrderCommand
 import com.munchies.order.domain.model.OrderId
-import com.munchies.order.domain.model.TakeawayInfo
 import com.munchies.order.domain.model.TakeawayOrder
 import com.munchies.order.domain.ports.OrderRepository
 
-class UpdateTakeawayOrderInfoUseCase(private val repository: OrderRepository) :
-  UpdateTakeawayOrderInfo {
+class UpdateTakeawayOrderInfoUseCase(
+  private val repository: OrderRepository,
+) : UpdateTakeawayOrderInfo {
+
   override fun execute(command: UpdateTakeawayOrderCommand): UpdateTakeawayOrderInfo.Result {
     val order = repository.findById(OrderId(command.orderId))
 
     return when {
       order == null -> OrderNotFound
       order.customerId.value != command.customerId -> Unauthorized
-      command.pickupTime <= System.currentTimeMillis() -> InvalidDate
-      else -> {
-        val updatedOrder = (order as TakeawayOrder).copy(
-          takeawayInfo = TakeawayInfo(
-            pickupTime = command.pickupTime,
-            customerName = command.customerName,
-          ),
-        )
-        repository.update(updatedOrder)
-        Success
+      order !is TakeawayOrder -> OrderNotFound
+      else -> when (val result = order.updateInfo(command.pickupTime, command.customerName)) {
+        is TakeawayOrder.UpdateResult.Failure.InvalidDate -> InvalidDate
+        is TakeawayOrder.UpdateResult.Success -> {
+          repository.update(result.order)
+          Success
+        }
       }
     }
   }
