ca98788fc9c729c62ccb6d909a36b3027d29cbbe
Norby99 <gabos.norbert99@gmail.com>
2026-06-10 13:55:18 +0200

    feat(order): implement UpdateTakeawayOrderInfoUseCase usecase

---
+++ order-service/src/main/kotlin/com/munchies/order/application/usecase/UpdateTakeawayOrderInfoUseCase.kt
@@ -0,0 +1,33 @@
+package com.munchies.order.application.usecase
+
+import com.munchies.order.application.port.inbound.UpdateTakeawayOrderInfo
+import com.munchies.order.application.port.inbound.UpdateTakeawayOrderInfo.Result.Failure.*
+import com.munchies.order.application.port.inbound.UpdateTakeawayOrderInfo.Result.Success
+import com.munchies.order.application.port.inbound.command.UpdateTakeawayOrderCommand
+import com.munchies.order.domain.model.OrderId
+import com.munchies.order.domain.model.TakeawayInfo
+import com.munchies.order.domain.model.TakeawayOrder
+import com.munchies.order.domain.ports.OrderRepository
+
+class UpdateTakeawayOrderInfoUseCase(private val repository: OrderRepository) :
+  UpdateTakeawayOrderInfo {
+  override fun execute(command: UpdateTakeawayOrderCommand): UpdateTakeawayOrderInfo.Result {
+    val order = repository.findById(OrderId(command.orderId))
+
+    return when {
+      order == null -> OrderNotFound
+      order.customerId.value != command.customerId -> Unauthorized
+      command.pickupTime <= System.currentTimeMillis() -> InvalidDate
+      else -> {
+        val updatedOrder = (order as TakeawayOrder).copy(
+          takeawayInfo = TakeawayInfo(
+            pickupTime = command.pickupTime,
+            customerName = command.customerName,
+          ),
+        )
+        repository.update(updatedOrder)
+        Success
+      }
+    }
+  }
+}
