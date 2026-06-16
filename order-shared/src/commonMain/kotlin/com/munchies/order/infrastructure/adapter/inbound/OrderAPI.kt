16974583282b0e62463242284195641253282d17
Norby99 <gabos.norbert99@gmail.com>
2026-06-16 18:34:52 +0200

    feat(order): declare OrderAPI

---
+++ order-shared/src/commonMain/kotlin/com/munchies/order/infrastructure/adapter/inbound/OrderAPI.kt
@@ -0,0 +1,32 @@
+package com.munchies.order.infrastructure.adapter.inbound
+
+sealed interface OrderAPI {
+
+  interface AdvanceOrderStatusAPI<Request, Response> : OrderAPI {
+    fun advanceOrderStatus(request: Request): Response
+  }
+
+  interface DiscardOrderAPI<Request, Response> : OrderAPI {
+    fun discardOrder(request: Request): Response
+  }
+
+  interface GetOrderDetailsAPI<Request, Response> : OrderAPI {
+    fun getOrderDetails(request: Request): Response
+  }
+
+  interface PlaceOrderAPI<Request, Response> : OrderAPI {
+    fun placeOrder(request: Request): Response
+  }
+
+  interface UpdateDeliveryOrderInfoAPI<Request, Response> : OrderAPI {
+    fun updateDeliveryOrderInfo(request: Request): Response
+  }
+
+  interface UpdateOrderItemsAPI<Request, Response> : OrderAPI {
+    fun updateOrderItems(request: Request): Response
+  }
+
+  interface UpdateTakeawayOrderInfoAPI<Request, Response> : OrderAPI {
+    fun updateTakeawayOrderInfo(request: Request): Response
+  }
+}
