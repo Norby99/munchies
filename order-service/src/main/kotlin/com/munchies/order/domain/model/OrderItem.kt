25e10e87b608f2beebe9f8371f8af32dd3e3be95
Norby99 <gabos.norbert99@gmail.com>
2026-06-18 14:44:05 +0200

    docs(order): add documentation to domain.model

--- order-service/src/main/kotlin/com/munchies/order/domain/model/OrderItem.kt
+++ order-service/src/main/kotlin/com/munchies/order/domain/model/OrderItem.kt
@@ -10,4 +10,11 @@ data class OrderItem(val menuItemId: MenuItemId, val quantity: Int) {
   fun isValid(): Boolean = quantity > 0
 }
 
+/**
+ * Represents the unique identifier for a menu item.
+ *
+ * This is an inline value class that wraps a string value representing the menu item ID.
+ *
+ * @property value The string representation of the menu item ID.
+ */
 @JvmInline value class MenuItemId(val value: String)
