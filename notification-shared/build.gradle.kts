8921c3f8e27a5fd257430fdce5c96cac870a7f84
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-06-19 17:32:06 +0200

    refactor: notification importable modules

--- notification-shared/build.gradle.kts
+++ notification-shared/build.gradle.kts
@@ -9,6 +9,22 @@ kotlin {
       customField("version", "0.1.0")
       customField("main", "kotlin/munchies-notification-shared.js")
       customField("types", "kotlin/munchies-notification-shared.d.ts")
+
+      customField(
+        "exports",
+        mapOf(
+          "." to mapOf(
+            "types" to "./kotlin/munchies-notification-shared.d.ts",
+            "default" to "./kotlin/munchies-notification-shared.js",
+            "import" to "./kotlin/munchies-notification-shared.js",
+          ),
+          "./kotlin/notification-modules" to mapOf(
+            "types" to "./kotlin/notification-modules.d.ts",
+            "default" to "./kotlin/notification-modules.js",
+            "import" to "./kotlin/notification-modules.js",
+          ),
+        ),
+      )
     }
   }
 }
