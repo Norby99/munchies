7e636aa985081dddd82f9107e572268baf8f2779
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-06-23 13:04:43 +0200

    refactor: importable types

---
+++ table-reservation-shared/src/jsMain/resources/table-reservation-modules.js
@@ -0,0 +1,7 @@
+const generated = require("./munchies-table-reservation-shared.js");
+const _table = generated.com.munchies.tablereservation;
+
+module.exports = {
+  TableReservationAPI:
+    _table.infrastructure.adapter.inbound.TableReservationAPI,
+};
