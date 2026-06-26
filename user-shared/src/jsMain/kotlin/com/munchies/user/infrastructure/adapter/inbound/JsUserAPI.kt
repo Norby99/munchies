f8b29b825d58547a5d371c3597b06bf3c4be65c3
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-06-26 19:26:35 +0200

    feat: gateway register user

--- user-shared/src/jsMain/kotlin/com/munchies/user/infrastructure/adapter/inbound/JsUserAPI.kt
+++ user-shared/src/jsMain/kotlin/com/munchies/user/infrastructure/adapter/inbound/JsUserAPI.kt
@@ -5,7 +5,7 @@ import com.munchies.user.infrastructure.adapter.dto.UserDTO
 import com.munchies.user.infrastructure.adapter.inbound.request.*
 import com.munchies.user.infrastructure.adapter.inbound.web.config.UserServiceConfig
 import com.munchies.user.infrastructure.adapter.outbound.response.GetUserResult
-import com.munchies.user.infrastructure.adapter.outbound.response.RegisterUserResponse
+import com.munchies.user.infrastructure.adapter.outbound.response.RegisterUserResult
 import kotlin.js.Promise
 
 @JsExport
@@ -16,34 +16,51 @@ abstract class JsGetUserAPI : UserAPI.GetUserAPI<Promise<GetUserResult>>, API() 
 }
 
 @JsExport
-abstract class JsRegisterUserAPI : UserAPI.RegisterUserAPI<Promise<RegisterUserResponse>>, API() {
-  override fun getPath(): String = UserServiceConfig.SERVICE_PATH + UserServiceConfig.REGISTER_USER_PATH
+abstract class JsRegisterUserAPI : UserAPI.RegisterUserAPI<Promise<RegisterUserResult>>, API() {
+  override fun getPath(): String =
+    UserServiceConfig.SERVICE_PATH + UserServiceConfig.REGISTER_USER_PATH
   override fun getPort(): Int = UserServiceConfig.SERVICE_PORT
-  abstract override fun registerUser(request: RegisterUserRequest): Promise<RegisterUserResponse>
+  abstract override fun registerUser(request: RegisterUserRequest): Promise<RegisterUserResult>
 }
 
 @JsExport
-abstract class JsLoginUserAPI : UserAPI.LoginUserAPI<LoginUserRequest, UserDTO> {
+abstract class JsLoginUserAPI : UserAPI.LoginUserAPI<LoginUserRequest, UserDTO>, API() {
+  override fun getPath(): String =
+    UserServiceConfig.SERVICE_PATH + UserServiceConfig.LOGIN_USER_PATH
+  override fun getPort(): Int = UserServiceConfig.SERVICE_PORT
   abstract override fun loginUser(request: LoginUserRequest): UserDTO
 }
 
 @JsExport
 abstract class JsUpdateUserPasswordAPI :
-  UserAPI.UpdateUserPasswordAPI<UpdateUserPasswordRequest, UserDTO> {
+  UserAPI.UpdateUserPasswordAPI<UpdateUserPasswordRequest, UserDTO>, API() {
+  override fun getPath(): String =
+    UserServiceConfig.SERVICE_PATH + UserServiceConfig.UPDATE_USER_PASSWORD_PATH
+  override fun getPort(): Int = UserServiceConfig.SERVICE_PORT
   abstract override fun updateUserPassword(request: UpdateUserPasswordRequest): UserDTO
 }
 
 @JsExport
-abstract class JsUpdateUserInfoAPI : UserAPI.UpdateUserInfoAPI<UpdateUserInfoRequest, UserDTO> {
+abstract class JsUpdateUserInfoAPI :
+  UserAPI.UpdateUserInfoAPI<UpdateUserInfoRequest, UserDTO>, API() {
+  override fun getPath(): String =
+    UserServiceConfig.SERVICE_PATH + UserServiceConfig.UPDATE_USER_INFO_PATH
+  override fun getPort(): Int = UserServiceConfig.SERVICE_PORT
   abstract override fun updateUserInfo(request: UpdateUserInfoRequest): UserDTO
 }
 
 @JsExport
-abstract class JsDeleteUserAPI : UserAPI.DeleteUserAPI<DeleteUserRequest, UserDTO> {
+abstract class JsDeleteUserAPI : UserAPI.DeleteUserAPI<DeleteUserRequest, UserDTO>, API() {
+  override fun getPath(): String =
+    UserServiceConfig.SERVICE_PATH + UserServiceConfig.DELETE_USER_PATH
+  override fun getPort(): Int = UserServiceConfig.SERVICE_PORT
   abstract override fun deleteUser(request: DeleteUserRequest): UserDTO
 }
 
 @JsExport
-abstract class JsEmailVerificationAPI : UserAPI.EmailVerificationAPI<UserDTO> {
+abstract class JsEmailVerificationAPI : UserAPI.EmailVerificationAPI<UserDTO>, API() {
+  override fun getPath(): String =
+    UserServiceConfig.SERVICE_PATH + UserServiceConfig.VERIFY_EMAIL_PATH
+  override fun getPort(): Int = UserServiceConfig.SERVICE_PORT
   abstract override fun verifyEmail(id: String, otk: String): UserDTO
 }
--- user-shared/src/jsMain/resources/user-modules.js
+++ user-shared/src/jsMain/resources/user-modules.js
@@ -6,12 +6,14 @@ const _outbound = _user.infrastructure.adapter.outbound;
 
 module.exports = {
   UserDTO: _user.infrastructure.adapter.dto.UserDTO,
+  
   GetUserAPI: _inbound.JsGetUserAPI,
   GetUserResponse: _outbound.response.GetUserResponse,
   getUserResponseFromJson: _outbound.response.getUserResponseFromJson,
   GetUserResult: _outbound.response.GetUserResult,
   GetUserSuccess: _outbound.response.GetUserSuccess,
   GetUserFailure: _outbound.response.GetUserFailure,
+  
   RegisterUserAPI: _inbound.JsRegisterUserAPI,
   RegisterUserResponse: _outbound.response.RegisterUserResponse,
   registerUserResponseFromJson: _outbound.response.registerUserResponseFromJson,
