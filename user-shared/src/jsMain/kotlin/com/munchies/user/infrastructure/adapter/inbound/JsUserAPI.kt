e374508dd3876ce4cff5ff96606aa80c4042a3ff
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-06-29 16:27:51 +0200

    fix: promise based response

--- user-shared/src/jsMain/kotlin/com/munchies/user/infrastructure/adapter/inbound/JsUserAPI.kt
+++ user-shared/src/jsMain/kotlin/com/munchies/user/infrastructure/adapter/inbound/JsUserAPI.kt
@@ -9,60 +9,62 @@ import com.munchies.user.infrastructure.adapter.outbound.response.*
 import kotlin.js.Promise
 
 @JsExport
-abstract class JsGetUserAPI : UserAPI.GetUserAPI<Promise<GetUserResult>>, API() {
+abstract class JsGetUserAPI : UserAPI.GetUserAPI<Promise<GetUserResponse>>, API() {
   override fun getMethod(): HttpMethod = HttpMethod.GET
   override fun getPath(): String = UserServiceConfig.SERVICE_PATH
   override fun getPort(): Int = UserServiceConfig.SERVICE_PORT
-  abstract override fun getUser(id: String): Promise<GetUserResult>
+  abstract override fun getUser(id: String): Promise<GetUserResponse>
 }
 
 @JsExport
-abstract class JsRegisterUserAPI : UserAPI.RegisterUserAPI<Promise<RegisterUserResult>>, API() {
+abstract class JsRegisterUserAPI : UserAPI.RegisterUserAPI<Promise<RegisterUserResponse>>, API() {
   override fun getPath(): String =
     UserServiceConfig.SERVICE_PATH + UserServiceConfig.REGISTER_USER_PATH
   override fun getPort(): Int = UserServiceConfig.SERVICE_PORT
   override fun getMethod(): HttpMethod = HttpMethod.POST
-  abstract override fun registerUser(request: RegisterUserRequest): Promise<RegisterUserResult>
+  abstract override fun registerUser(request: RegisterUserRequest): Promise<RegisterUserResponse>
 }
 
 @JsExport
-abstract class JsLoginUserAPI : UserAPI.LoginUserAPI<LoginUserResponse>, API() {
+abstract class JsLoginUserAPI : UserAPI.LoginUserAPI<Promise<LoginUserResponse>>, API() {
   override fun getPath(): String =
     UserServiceConfig.SERVICE_PATH + UserServiceConfig.LOGIN_USER_PATH
   override fun getPort(): Int = UserServiceConfig.SERVICE_PORT
   override fun getMethod(): HttpMethod = HttpMethod.POST
-  abstract override fun loginUser(request: LoginUserRequest): LoginUserResponse
+  abstract override fun loginUser(request: LoginUserRequest): Promise<LoginUserResponse>
 }
 
 @JsExport
 abstract class JsUpdateUserPasswordAPI :
-  UserAPI.UpdateUserPasswordAPI<UpdateUserPasswordResponse>, API() {
+  UserAPI.UpdateUserPasswordAPI<Promise<UpdateUserPasswordResponse>>, API() {
   override fun getPath(): String =
     UserServiceConfig.SERVICE_PATH + UserServiceConfig.UPDATE_USER_PASSWORD_PATH
   override fun getPort(): Int = UserServiceConfig.SERVICE_PORT
   override fun getMethod(): HttpMethod = HttpMethod.PATCH
   abstract override fun updateUserPassword(
     request: UpdateUserPasswordRequest,
-  ): UpdateUserPasswordResponse
+  ): Promise<UpdateUserPasswordResponse>
 }
 
 @JsExport
 abstract class JsUpdateUserInfoAPI :
-  UserAPI.UpdateUserInfoAPI<UpdateUserInfoResponse>, API() {
+  UserAPI.UpdateUserInfoAPI<Promise<UpdateUserInfoResponse>>, API() {
   override fun getPath(): String =
     UserServiceConfig.SERVICE_PATH + UserServiceConfig.UPDATE_USER_INFO_PATH
   override fun getPort(): Int = UserServiceConfig.SERVICE_PORT
   override fun getMethod(): HttpMethod = HttpMethod.PATCH
-  abstract override fun updateUserInfo(request: UpdateUserInfoRequest): UpdateUserInfoResponse
+  abstract override fun updateUserInfo(
+    request: UpdateUserInfoRequest,
+  ): Promise<UpdateUserInfoResponse>
 }
 
 @JsExport
-abstract class JsDeleteUserAPI : UserAPI.DeleteUserAPI<DeleteUserResponse>, API() {
+abstract class JsDeleteUserAPI : UserAPI.DeleteUserAPI<Promise<DeleteUserResponse>>, API() {
   override fun getPath(): String =
     UserServiceConfig.SERVICE_PATH + UserServiceConfig.DELETE_USER_PATH
   override fun getPort(): Int = UserServiceConfig.SERVICE_PORT
   override fun getMethod(): HttpMethod = HttpMethod.DELETE
-  abstract override fun deleteUser(request: DeleteUserRequest): DeleteUserResponse
+  abstract override fun deleteUser(request: DeleteUserRequest): Promise<DeleteUserResponse>
 }
 
 @JsExport
