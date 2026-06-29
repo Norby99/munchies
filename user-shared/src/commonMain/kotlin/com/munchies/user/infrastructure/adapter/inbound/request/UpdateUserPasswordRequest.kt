8176bc5fafc04ff1e6adcec211299a1909774e89
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-06-29 16:45:24 +0200

    feat: update user password gateway endpoint

--- user-shared/src/commonMain/kotlin/com/munchies/user/infrastructure/adapter/inbound/request/UpdateUserPasswordRequest.kt
+++ user-shared/src/commonMain/kotlin/com/munchies/user/infrastructure/adapter/inbound/request/UpdateUserPasswordRequest.kt
@@ -1,12 +1,18 @@
 package com.munchies.user.infrastructure.adapter.inbound.request
 
 import kotlin.js.JsExport
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @JsExport
+@Serializable
 data class UpdateUserPasswordRequest(
   val id: String,
   val username: String = "",
   val email: String = "",
   val oldHashedPassword: String,
   val newPassword: String,
-)
+) {
+  fun toJson(): String = Json.encodeToString(this)
+}
