24c1372339eea900c19751b39e198f7b0108e413
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-06-29 16:35:41 +0200

    feat: update user info gateway endpoint

--- gateway-service/src/main/ts/infrastructure/adapter/inbound/web/services/user.ts
+++ gateway-service/src/main/ts/infrastructure/adapter/inbound/web/services/user.ts
@@ -118,3 +118,44 @@ export class LoginUser extends LoginUserAPI{
             return Promise.resolve(new LoginUserResponse(new LoginUserFailure(JSON.stringify(e))));
           }  }
 }
+
+
+
+
+import {
+  UpdateUserInfoAPI,
+  UpdateUserInfoRequest,
+  UpdateUserInfoResponse,
+  UpdateUserInfoResult,
+  UpdateUserInfoSuccess,
+  UpdateUserInfoFailure,
+  updateUserInfoResponseFromJson
+} from "munchies-user-service-shared/kotlin/user-modules"
+export class UpdateUserInfo extends UpdateUserInfoAPI{
+  updateUserInfo(request: UpdateUserInfoRequest): Promise<UpdateUserInfoResponse> {
+    try {
+              const uri = process.env.USER_SERVICE_URL;
+              if (!uri) throw new Error("USER_SERVICE_URL is not defined in .env");
+            
+              const result = axios.post(
+                uri + this.getPath(),
+                request.toJson()
+              ).then(value => {
+                const response = updateUserInfoResponseFromJson(JSON.stringify(value.data))
+                if (response.result.type != "UpdateUserInfoSuccess")
+                  return new UpdateUserInfoResponse(response.result as UpdateUserInfoFailure);
+                return new UpdateUserInfoResponse(response.result as UpdateUserInfoSuccess);
+                }).catch(e => {
+                  console.log("axios error: " + JSON.stringify(e).toString());
+                  return new UpdateUserInfoResponse(new UpdateUserInfoFailure(e));
+                });
+                  
+              return result;
+                    
+              }
+              catch(e: any){
+                console.log("Something went wrong");
+                console.log("error is " + JSON.stringify(e));
+                return Promise.resolve(new UpdateUserInfoResponse(new UpdateUserInfoFailure(JSON.stringify(e))));
+              }  }
+}
