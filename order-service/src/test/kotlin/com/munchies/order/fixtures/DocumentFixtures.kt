6c66da3f3f1d52490c44bf1deaa890077e7c6823
Norby99 <gabos.norbert99@gmail.com>
2026-07-06 17:17:03 +0200

    docs(order): add doc to DocumentFixtures

--- order-service/src/test/kotlin/com/munchies/order/fixtures/DocumentFixtures.kt
+++ order-service/src/test/kotlin/com/munchies/order/fixtures/DocumentFixtures.kt
@@ -8,6 +8,16 @@ import com.munchies.order.infrastructure.adapter.outbound.mongo.document.OrderIt
 import com.munchies.order.infrastructure.adapter.outbound.mongo.document.TableInfoDocument
 import com.munchies.order.infrastructure.adapter.outbound.mongo.document.TakeawayInfoDocument
 
+/**
+ * Creates a sample OrderDocument for testing purposes.
+ *
+ * @param orderType The type of the order (e.g., DELIVERY, DINE_IN, TAKEAWAY).
+ * @param status The status of the order (e.g., PENDING, COMPLETED).
+ * @param deliveryInfo Optional delivery information for delivery orders.
+ * @param tableInfo Optional table information for dine-in orders.
+ * @param takeawayInfo Optional takeaway information for takeaway orders.
+ * @return An instance of OrderDocument with the specified parameters.
+ */
 fun createDeliveryOrderDocument(
   orderType: String = OrderType.DELIVERY.name,
   status: String = OrderStatus.PENDING.name,
@@ -29,6 +39,14 @@ fun createDeliveryOrderDocument(
   takeawayInfo = null,
 )
 
+/**
+ * Creates a sample DineInOrderDocument for testing purposes.
+ *
+ * @param orderType The type of the order (default is DINE_IN).
+ * @param status The status of the order (default is PENDING).
+ * @param tableInfo Optional table information for dine-in orders.
+ * @return An instance of OrderDocument representing a dine-in order.
+ */
 fun createDineInOrderDocument(
   orderType: String = OrderType.DINE_IN.name,
   status: String = OrderStatus.PENDING.name,
@@ -48,6 +66,14 @@ fun createDineInOrderDocument(
   takeawayInfo = null,
 )
 
+/**
+ * Creates a sample TakeawayOrderDocument for testing purposes.
+ *
+ * @param orderType The type of the order (default is TAKEAWAY).
+ * @param status The status of the order (default is PENDING).
+ * @param takeawayInfo Optional takeaway information for takeaway orders.
+ * @return An instance of OrderDocument representing a takeaway order.
+ */
 fun createTakeawayOrderDocument(
   orderType: String = OrderType.TAKEAWAY.name,
   status: String = OrderStatus.PENDING.name,
@@ -67,6 +93,13 @@ fun createTakeawayOrderDocument(
   takeawayInfo = takeawayInfo,
 )
 
+/**
+ * Creates a sample OrderItemDocument for testing purposes.
+ *
+ * @param menuItemId The ID of the menu item (default is the default order ID).
+ * @param quantity The quantity of the menu item (default is 1).
+ * @return An instance of OrderItemDocument with the specified parameters.
+ */
 fun createOrderItemDocument(menuItemId: String = defaultOrderId.value, quantity: Int = 1) =
   OrderItemDocument(
     menuItemId = menuItemId,
