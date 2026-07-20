104a1456fb21d2def3ae6c89135f43417757b392
Norby99 <gabos.norbert99@gmail.com>
2026-07-20 14:09:28 +0200

    fix(dokka): proviamo wombo-combo

--- build-logic/src/main/kotlin/dokka-convention.gradle.kts
+++ build-logic/src/main/kotlin/dokka-convention.gradle.kts
@@ -1,4 +1,3 @@
-
 import utils.ProjectType
 import utils.getProjectType
 import utils.getServiceName
@@ -8,34 +7,10 @@ plugins {
 }
 
 if (project.getProjectType() == ProjectType.SERVICE) {
-
   dokka {
     moduleName = getServiceName(project) + "-" + project.getProjectType().toString().lowercase()
-  }
-
-  dokka {
     dokkaPublications.html {
-      outputDirectory.set(
-        rootProject.layout.buildDirectory
-          .dir("docs/html"),
-      )
       failOnWarning = true
-    }
-  }
-
-  if (project.getProjectType() == ProjectType.SERVICE) {
-    project.tasks.dokkaGeneratePublicationHtml {
-      for (item in rootProject.subprojects) {
-        if (item.getProjectType() == ProjectType.SHARED) {
-          val sharedName = "${getServiceName(item)}-shared"
-          val jsDokkaTaskName = "dokkaGeneratePublicationHtml"
-          val sharedProject = project(":$sharedName")
-
-          if (sharedProject.tasks.findByName(jsDokkaTaskName) != null) {
-            dependsOn(":$sharedName:$jsDokkaTaskName")
-          }
-        }
-      }
     }
   }
 }
