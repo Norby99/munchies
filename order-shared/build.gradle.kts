858bb2a8a40b72c4566f6c7f0436304b1744b507
Norby99 <gabos.norbert99@gmail.com>
2026-07-22 17:44:44 +0200

    feat(order): create modules d ts for DTOs

--- order-shared/build.gradle.kts
+++ order-shared/build.gradle.kts
@@ -7,8 +7,28 @@ kotlin {
     compilations["main"].packageJson {
       customField("name", "munchies-order-service-shared")
       customField("version", "0.1.0")
-      customField("main", "kotlin/munchies-order-service-shared.js")
-      customField("types", "kotlin/munchies-order-service-shared.d.ts")
+      customField("main", "kotlin/munchies-order-shared.js")
+      customField("types", "kotlin/munchies-order-shared.d.ts")
+
+      customField(
+        "exports",
+        mapOf(
+          "." to mapOf(
+            "types" to "./kotlin/munchies-order-shared.d.ts",
+            "default" to "./kotlin/munchies-order-shared.js",
+            "import" to "./kotlin/munchies-order-shared.js",
+          ),
+          "./kotlin/order-modules" to mapOf(
+            "types" to "./kotlin/order-modules.d.ts",
+            "default" to "./kotlin/order-modules.js",
+            "import" to "./kotlin/order-modules.js",
+          ),
+        ),
+      )
     }
   }
+}
+
+dependencies {
+  commonMainImplementation(project(":commons"))
 }
