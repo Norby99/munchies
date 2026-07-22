95e0ff9d0f092d6baaf6f0d9a6b61e21c9125be2
Norby99 <gabos.norbert99@gmail.com>
2026-07-22 17:44:44 +0200

    feat(order): create modules d ts for DTOs

---
+++ order-shared/src/jsMain/resources/order-modules.d.ts
@@ -0,0 +1,23 @@
+import type {com} from "./munchies-order-shared";
+
+export type OrderDTO =
+    com.munchies.order.infrastructure.adapter.dto.OrderDto;
+export declare const OrderDTO: typeof com.munchies.order.infrastructure.adapter.dto.OrderDto;
+
+export type DeliveryDTO =
+    com.munchies.order.infrastructure.adapter.dto.Delivery;
+export declare const DeliveryDTO: typeof com.munchies.order.infrastructure.adapter.dto.Delivery;
+
+export type TakeawayDTO =
+    com.munchies.order.infrastructure.adapter.dto.Takeaway;
+export declare const TakeawayDTO: typeof com.munchies.order.infrastructure.adapter.dto.Takeaway;
+
+export type DineInDTO =
+    com.munchies.order.infrastructure.adapter.dto.DineIn;
+export declare const DineInDTO: typeof com.munchies.order.infrastructure.adapter.dto.DineIn;
+
+export type OrderType = com.munchies.order.infrastructure.adapter.dto.OrderType;
+export declare const OrderType: typeof com.munchies.order.infrastructure.adapter.dto.OrderType;
+
+export type OrderItemDto = com.munchies.order.infrastructure.adapter.dto.OrderItemDto;
+export declare const OrderItemDto: typeof com.munchies.order.infrastructure.adapter.dto.OrderItemDto;
