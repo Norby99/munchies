5e21b88e54b708c110670c38341ea5d5ab91bef3
erattini <emirattinee@gmail.com>
2026-07-24 15:57:59 +0200

    test: fix repos injection for cucumber test environment, remove a newline

--- restaurant-service/src/main/kotlin/com/munchies/restaurant/infrastructure/persistence/InMemoryMenuRepository.kt
+++ restaurant-service/src/main/kotlin/com/munchies/restaurant/infrastructure/persistence/InMemoryMenuRepository.kt
@@ -3,9 +3,17 @@ package com.munchies.restaurant.infrastructure.persistence
 import com.munchies.commons.repository.InMemoryRepository
 import com.munchies.restaurant.domain.aggregate.Menu
 import com.munchies.restaurant.domain.aggregate.MenuId
+import com.munchies.restaurant.domain.repository.MenuRepository
+import com.munchies.restaurant.domain.valueobject.RestaurantId
 import io.micronaut.context.annotation.Requires
 import jakarta.inject.Singleton
 
 @Singleton
 @Requires(env = ["test"])
-class InMemoryMenuRepository : InMemoryRepository<MenuId, Menu>()
+class InMemoryMenuRepository : MenuRepository, InMemoryRepository<MenuId, Menu>() {
+  override fun findByIdAndRestaurantId(id: MenuId, restaurantId: RestaurantId): Menu? =
+    findById(id)?.takeIf { it.restaurantId == restaurantId }
+
+  override fun findAllByRestaurantId(restaurantId: RestaurantId): List<Menu> =
+    findAllByPredicate { it.restaurantId == restaurantId }
+}
