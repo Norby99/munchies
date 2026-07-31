0b151371d78c89c7f3c9aebae641de1ef2e10691
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-07-31 22:00:11 +0200

    fix: working refactored get-user and register-user

--- gateway-service/src/main/ts/infrastructure/adapter/middleware/routes/user/register-user.route.ts
+++ gateway-service/src/main/ts/infrastructure/adapter/middleware/routes/user/register-user.route.ts
@@ -14,8 +14,9 @@ import {
   RegisterUserAPI,
   RegisterUserResponse,
   UserServiceConfig,
+  UserDTO,
 } from "munchies-user-service-shared/kotlin/user-modules";
-import { AuthedRequest } from "../../auth";
+import { AuthedRequest, injectCookie, parseAuthRoleString } from "../../auth";
 import { axiosClient, request } from "../internal-client";
 import { fillPath } from "../routes";
 import { createSimpleRoute, SimpleRoute } from "../simple-route";
@@ -38,10 +39,13 @@ export class RegisterUserRoute
   authRole: AuthRole | null;
 
   async registerUser(
-    req: RegisterUserRequest
+    req: RegisterUserRequest,
   ): Promise<RegisterUserResponse | ErrorResponse> {
     const uri = process.env.USER_SERVICE_URL;
-    if (!uri) return Promise.resolve(new ErrorResponse("Missing User Service URL", 500));
+    if (!uri)
+      return Promise.resolve(
+        new ErrorResponse("Missing User Service URL", 500),
+      );
 
     const response = request<RegisterUserResponse>(
       uri + this.path,
@@ -55,21 +59,43 @@ export class RegisterUserRoute
   }
 
   private handler: {
-    forward: (req: AuthedRequest) => Promise<RegisterUserResponse | ErrorResponse>;
+    forward: (
+      req: AuthedRequest,
+    ) => Promise<RegisterUserResponse | ErrorResponse>;
     respond: RequestHandler;
   } = {
     forward: async (req: AuthedRequest) => {
       try {
-        console.log("Register forwards handler | string: " + String(req.body))
-        const registerReq = this.parseRequest(req.body);
+        const registerReq = this.parseRequest(String(req.body));
         return this.registerUser(registerReq);
       } catch (err: any) {
         return new ErrorResponse("RegisterUser forward: \n" + String(err), 500);
       }
     },
     respond: async (req: Request, res: Response) => {
-      const result = await this.forward(req as AuthedRequest);
-      res.status(result.code).type("json").send(result.toJson());
+      const response = await this.forward(req as AuthedRequest);
+      const result = response.result;
+      if (result instanceof UserDTO) {
+        try {
+          injectCookie(res, {
+            id: result.id,
+            role: parseAuthRoleString(result.role),
+          });
+        } catch (e: any) {
+          res
+            .status(500)
+            .type("json")
+            .send(
+              new ErrorResponse(
+                "Something went wrong during cookie injection: " + String(e),
+                500,
+              ).toJson(),
+            );
+          return;
+        }
+      }
+
+      res.status(response.code).type("json").send(response.toJson());
     },
   };
 
