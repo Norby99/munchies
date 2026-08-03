3f1f4d615c2c73d2de6a8aa88ef89324cefad838
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-08-03 16:25:07 +0200

    chore: removed postman stuff

--- curl-request.sh
+++
@@ -1,5 +0,0 @@
-# Salva il cookie in locale
-curl -c testCookie.txt --data-binary @testBody.txt -H "Content-Type: application/json" -X POST "http://localhost:8086/users/register"
-
-# Carica il cookie da locale
-curl -b testCookie.txt --data-binary @testBody.txt -H "Content-Type: application/json" -X GET "http://localhost:8086/users/"
