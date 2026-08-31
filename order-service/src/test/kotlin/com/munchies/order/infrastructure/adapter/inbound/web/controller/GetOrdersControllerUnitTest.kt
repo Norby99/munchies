6bc9a5ef1bb82deabe5e040bce5945b1d5f253d6
Norby99 <gabos.norbert99@gmail.com>
2026-08-31 13:13:50 +0200

    chore(order): minor fix

--- order-service/src/test/kotlin/com/munchies/order/infrastructure/adapter/inbound/web/controller/GetOrdersControllerUnitTest.kt
+++ order-service/src/test/kotlin/com/munchies/order/infrastructure/adapter/inbound/web/controller/GetOrdersControllerUnitTest.kt
@@ -5,10 +5,12 @@ import com.munchies.order.fixtures.createDeliveryOrder
 import com.munchies.order.fixtures.createTakeawayOrder
 import com.munchies.order.fixtures.defaultRestaurantId
 import com.munchies.order.infrastructure.adapter.dto.factory.OrderDtoFactory.toDto
+import com.munchies.order.infrastructure.adapter.inbound.web.controller.exception.NotFoundException
 import io.kotest.matchers.shouldBe
 import io.micronaut.http.HttpStatus
 import io.mockk.every
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class GetOrdersControllerUnitTest : BaseOrderController() {
 
@@ -35,13 +37,14 @@ class GetOrdersControllerUnitTest : BaseOrderController() {
     every { getOrders.execute(any()) } returns
       GetOrders.Result.Failure.OrderNotFound
 
-    val response = controller.getOrders(
-      restaurantId = defaultRestaurantId.value,
-      customerId = null,
-      orderStatus = null,
-    )
+    val exception = assertThrows<NotFoundException> {
+      controller.getOrders(
+        restaurantId = defaultRestaurantId.value,
+        customerId = null,
+        orderStatus = null,
+      )
+    }
 
-    response.status shouldBe HttpStatus.NOT_FOUND
-    response.body().code shouldBe HttpStatus.NOT_FOUND.code
+    exception.message shouldBe "No orders found matching the provided filters"
   }
 }
